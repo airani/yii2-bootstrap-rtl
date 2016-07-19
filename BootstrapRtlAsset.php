b8e15f7b732c3d133eecfd32311b37c2a7d57ba8
Ali Irani <irani.2007@gmail.com>
2016-07-19 11:48:51 +0430

    Update asset bundle with depends to bootstrap

--- BootstrapRtlAsset.php
+++ BootstrapRtlAsset.php
@@ -12,7 +12,12 @@ use yii\web\AssetBundle;
 class BootstrapRtlAsset extends AssetBundle
 {
     public $sourcePath = '@bower/bootstrap-rtl/dist';
+
     public $css = [
         'css/bootstrap-rtl.css',
+    ];
+
+    public $depends = [
+        'yii\bootstrap\BootstrapPluginAsset',
     ];
 }
